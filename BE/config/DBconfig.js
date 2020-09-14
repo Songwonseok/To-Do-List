3a0962a10ce7b9e4b404db2d3cab059a37554e6a
Wonseok Song <dnjstjr0517@gmail.com>
2020-09-14 23:06:32 +0900

    feat: ncloud 환경설정, mysql 설치 및 user get,post 구현
    - 엔클라우드 서버 생성 및 설정 완료
    - 서버에 mysql 설치 및 설정 완료
    - express-generator로 BE, FE app 생성
    - mysql2 설치 및 연동 확인
    - user get, post 라우터 구현

---
+++ BE/config/DBconfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: '49.50.163.212',
+    user: 'sws',
+    password: 'boostcamp',
+    database: 'todolist'
+};
---
+++ BE/routes/users.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql2');
+const DBconfig = require('../config/DBconfig');
+
+const connection = mysql.createConnection(DBconfig);
+const resObject = require('../resObject')
+
+
+router.get('/', (req, res, next) => {
+  console.log('bb');
+  const query = "SELECT * FROM users";
+  connection.query(query, (err, rows, fields) => {
+    console.log(err);
+    const response = resObject(200, true, '유저 조회 성공', rows);
+    res.send(response);
+  })
+})
+
+router.get('/:user_id', (req, res, next) => {
+  const query = "SELECT * FROM users where user_id = ?";
+  const user_id = req.params.user_id;
+  connection.query(query, user_id, (err, rows, fields) => {
+    console.log(err);
+    if (err) {
+      const response = resObject(400, false, err.sqlMessage, null);
+      res.send(response);
+    } 
+    const response = resObject(200, true, '유저 조회 성공', rows[0]);
+    res.send(response);
+  })
+})
+
+router.post('/', (req,res,next) => {
+  const query = "INSERT INTO users(email, password, name) VALUES(?,?,?)";
+  const params = [req.body.email, req.body.password, req.body.name];
+  connection.query(query, params, (err,rows,fields) => {
+    if(err){
+      const response = resObject(400, false, err.sqlMessage, null);
+      res.send(response);
+    }
+    const post_id = rows.insertId;
+    const response = resObject(200, true, '유저 등록 성공', { post_id });
+    res.send(response);
+  })
+})
+
+module.exports = router;
