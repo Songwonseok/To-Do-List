8779a0dd4f7407a2e86a604aac07a4f5155d2fef
Wonseok Song <dnjstjr0517@gmail.com>
2020-09-16 22:42:41 +0900

    Update: user/board/:userId 반환값 수정
    - 한 유저가 가지고 있는 모든 board와 card정보를 리턴함
    - card는 연결리스트로 정렬되어서 리턴

--- server/services/users.service.js
+++ server/services/users.service.js
@@ -1,5 +1,6 @@
 const UsersModel = require('../models/users.model')
 const BoardModel = require('../models/board.model')
+const CardModel = require('../models/card.model');
 const bcrypt = require('bcrypt');
 const saltRounds = 10; // 값이 높을 수록 비용 소요가 큼
 
@@ -7,6 +8,7 @@ class UsersService {
     constructor() {
         this.usersModel = new UsersModel();
         this.boardModel = new BoardModel();
+        this.cardModel = new CardModel();
     }
 
     async findAll() {
@@ -30,6 +32,12 @@ class UsersService {
     async getBoard(user_id) {
         try {
             const boards = await this.boardModel.SELECT_ALL(user_id);
+            
+            for (let i = 0; i < boards.length;i++){
+                const cardList = await this.cardModel.SELECT_ALL(boards[i].id);
+                const list = UsersService.getOrderList(boards[i], cardList);
+                boards[i].list = list;
+            }
             return boards;
         } catch (err) {
             throw err;
@@ -56,6 +64,19 @@ class UsersService {
                 });
             });
         })
+    }
+
+    static getOrderList(board, cardList) {
+        const list = [];
+        if (board.head) {
+            let node = cardList.find(c => c.id == board.head);
+            list.push(node);
+            while (node.next_card) {
+                node = cardList.find(c => c.id == node.next_card);
+                list.push(node);
+            }
+        }
+        return list;
     }
 }
 
