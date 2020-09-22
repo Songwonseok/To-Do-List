be24747419df9f5372b1966733916ec697d3dbc7
Wonseok Song <dnjstjr0517@gmail.com>
2020-09-22 23:25:24 +0900

    Feat: 회원가입 페이지 구현
    - 로그인화면에서 회원가입 페이지로 이동하는 a태그 추가
    - 회원가입 성공시 메인화면으로 이동

---
+++ client/src/javascripts/signup.js
@@ -0,0 +1,39 @@
+const { $, $All } = require('./utils')
+const signupForm = $('.signupForm');
+
+const signupEventHandler = (event) => {
+    event.preventDefault()
+    const payload = {
+        email: signupForm.email.value,
+        password: signupForm.password.value,
+        name: signupForm.name.value,
+        phone: signupForm.phone.value
+    };
+
+    fetch('/api/users', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            alert('회원가입 되었습니다.')
+            window.location.replace('/');
+        })
+        .catch((err) => {
+            alert(err);
+            window.location.replace('/signup');
+        })
+
+
+}
+
+const init = () => {
+
+    signupForm.addEventListener('submit', signupEventHandler);
+}
+
+init();
